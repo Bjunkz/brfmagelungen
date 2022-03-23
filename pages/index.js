290f59108904be80d5ac8e9e8516b63449d0d5fb
Bjorn <info@pianobjorn.com>
2022-03-23 17:20:01 +0100

    add css files

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 
 export default function Home() {
   return (
-    <div>brf magelungen</div>
+    <div className='test'>brf magelungen</div>
     
   )
 }
