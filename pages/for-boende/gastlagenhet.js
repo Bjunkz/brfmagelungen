cf2d070d443be5ba116d12052d2b6c86bd51a8c6
Bjorn <info@pianobjorn.com>
2022-05-10 09:30:47 +0200

    add content to gästlägenhet and gym och bastu page

--- pages/for-boende/gastlagenhet.js
+++ pages/for-boende/gastlagenhet.js
@@ -1,19 +1,54 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import PageHeader from "../../components/PageHeader";
 
 export default function guestApartment() {
   return (
     <>
-        <Head>
-        <title>För Boende</title>
+      <Head>
+        <title>Gästlägenhet</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-    <div>
-        <h1>Gästlägenhet</h1>
-        </div>
+
+      <div className="page-hero-placeholder"></div>
+
+      <PageHeader
+        breadcrumbs={[
+          { text: "Hem", URL: "/" },
+          { text: "För boende", URL: "/for-boende" },
+        ]}
+        title={"Gästlägenhet"}
+      />
+      <div className="page-intro-text">
+        <span className="bold-text">
+          Boende kan hyra gästlägenheten som ligger på Skebokvarnsvägen 271.
+        </span>{" "}
+        Kontakta fastighetsexpeditionen för bokning.
+        <br />
+      </div>
+
+      <div className="spacing medium"></div>
+
+      <div className="page-more-text">
+        Nyckelbricka och inbetalningskort hämtas på fastighetsexpeditionen.
+        <br></br>
+        Kostnad 300 kronor/dygn, max 7 dagar i följd.<br></br> Deposition 500
+        kronor som återfås efter kontroll av lägenheten. <br></br>Inflyttning kl
+        14:00 uthyrningsdagen.<br></br> utflyttning kl 12:00 avflyttningsdagen.
+        <br />
+        <div className="spacing small"></div>
+        I lägenheten finns 2 sängplatser, 2 sängplatser i bäddsoffa, kuddar och
+        täcken. Sänglinne och handdukar håller man med själv.
+        <br />
+        <div className="spacing small"></div>
+        Lägenheten är utrustad med tallrikar, bestick, glas o muggar, TV, radio,
+        kaffebryggare, vattenkokare, mikro, spishäll, kylskåp, dush och toalett.
+        <br />
+        Lägenheten skall vid avresa vara väl städad och lämnas i samma skick som
+        vid uthyrningstillfället. Nyckelbricka återlämnas till
+        fastighetsexpeditionen.
+      </div>
     </>
-    
-  )
+  );
 }
--- pages/for-boende/gym-och-bastu.js
+++ pages/for-boende/gym-och-bastu.js
@@ -1,19 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import PageHeader from "../../components/PageHeader";
 
 export default function gymAndSauna() {
   return (
     <>
-        <Head>
-        <title>För Boende</title>
+      <Head>
+        <title>Gym och Bastu</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-    <div>
-        <h1>Gym och bastu</h1>
-        </div>
+
+      <div className="page-hero-placeholder"></div>
+
+      <PageHeader
+        breadcrumbs={[
+          { text: "Hem", URL: "/" },
+          { text: "För boende", URL: "/for-boende" },
+        ]}
+        title={"Gym och Bastu"}
+      />
+      <div className="page-intro-text">
+        <span className="bold-text">
+          Föreningen har ett gym på Skebokvarnsvägen 298 och en bastu på
+          Skebokvarnsvägen 294 (igång från gaveln).
+        </span>{" "}
+        För närvarande är gymmet stängt pga fuktskada. Styrelsens förhoppning är
+        att kunna öppna gymmet till hösten 2022. Har du tips eller önskemål om
+        utrustning eller liknande vänligen{" "}
+        <Link href="/kontakt">
+          <a className="page-intro-text-link">kontakta styrelsen</a>
+        </Link>
+        <br />
+        Bastun går att boka tre timmar i taget mellan 07.00-22.00. Bokning görs
+        på panelen som sitter på väggen utanför entrén till bastun.
+      </div>
+
+      <div className="spacing medium"></div>
     </>
-    
-  )
+  );
 }
